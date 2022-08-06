06ae8bab3c573c64b8c7bac867dfcfb85dd6d8cc
Omar <o.moreta@gmail.com>
2022-08-06 17:15:46 -0400

    google auth login and log out React app JWT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import jwt_decode from "jwt-decode";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [user, setUser] = useState({});
+
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+    let userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
+  }
+
+  function handleSignOut(event) {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  }
+
+  useEffect(() => {
+    const REACT_APP_CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+    /* global google */
+    google.accounts.id.initialize({
+      //PLEASE MAKE AN ENV VARIABLE FOR CLIENT ID
+      client_id: `${REACT_APP_CLIENT_ID}`, // "*********PLACE YOUR ********* GOOGLE CLIENT ID********* HERE*********",
+      callback: handleCallbackResponse,
+    });
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+    google.accounts.id.prompt();
+  }, []);
+  // if we have no user: sign in button
+  // if we have a user: show the log out button
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="signInDiv"></div>
+      {Object.keys(user).length !== 0 && (
+        <button onClick={(e) => handleSignOut(e)}>Sign Out</button>
+      )}
+      {user && (
+        <div>
+          <img src={user.picture} alt=""></img>
+          <h3>{user.name}</h3>
+        </div>
+      )}
     </div>
   );
 }
